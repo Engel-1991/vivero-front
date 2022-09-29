30cbdcf2e1fc1493d1a09c48a148d8af9a51542d
miguel <gomezengel05@gmail.com>
2022-09-30 01:22:47 +0200

    Updates

---
+++ precache-manifest.5f4fb46e5ce6d3f06e7a8383f523490c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc66867478ae4a24d47af45ade77007a",
+    "url": "/vivero-front/index.html"
+  },
+  {
+    "revision": "a4950b78ba28126f609f",
+    "url": "/vivero-front/static/css/main.a84997f2.chunk.css"
+  },
+  {
+    "revision": "dfc53caca017d238fd5e",
+    "url": "/vivero-front/static/js/2.69284f5f.chunk.js"
+  },
+  {
+    "revision": "a4950b78ba28126f609f",
+    "url": "/vivero-front/static/js/main.816a36a7.chunk.js"
+  },
+  {
+    "revision": "0e624c643fbca06badd6",
+    "url": "/vivero-front/static/js/runtime-main.cb8d06bb.js"
+  },
+  {
+    "revision": "33e80f8322ed77263e01cafd0ff6bf8b",
+    "url": "/vivero-front/static/media/Pgloriosum.33e80f83.jpg"
+  }
+]);
